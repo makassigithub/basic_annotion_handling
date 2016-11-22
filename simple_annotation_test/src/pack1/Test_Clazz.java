67567409d60a93e87584d3c257b093fb05df5231
Makassi Zoulou <makassi@live.fr>
2016-11-21 21:02:34 -0500

    @commit numb_2

--- simple_annotation_test/src/pack1/Test_Clazz.java
+++ simple_annotation_test/src/pack1/Test_Clazz.java
@@ -20,8 +20,8 @@ public class Test_Clazz {
 	Interf itf = m.getAnnotation(Interf.class);
 	
 	//get the attribute values from the interface and print
-	  System.out.println("The Attr. class value in the first method :" +itf.method_1()+ "\n" +
-			 " The Attr. class value in the first method :" + itf.method_2());
+	  System.out.println("The Attr. value in the first method :" +itf.method_1()+ "\n" +
+			 "The Attr. value in the first method :" + itf.method_2());
 	
 		
 		
@@ -29,9 +29,40 @@ public class Test_Clazz {
 			System.out.println(nse.getMessage());
 		}
 	}
+	
+	// the second method takes parameters
+	@Interf(method_1 = "the second meth with param", method_2 = 13)
+	public static void this_meth2(String s, int i){
+		
+		// Instance of the class that contains the methods
+		Test_Clazz tc = new Test_Clazz();
+		
+		try{
+			// The Class object of the Test_Clazz instance
+			Class <?> cl = tc.getClass();
+			
+			// get the annotated Method object.Must explicitly provide the parameter list
+			Method m = cl.getMethod("this_meth2", String.class, int.class);
+			
+			//get the annotation from the method. 
+			// Interf.class is an  a Class object of the interface 
+			Interf intf = m.getAnnotation(Interf.class);
+			
+			//get and print the annotations
+			
+			System.out.println("method_2 attr1 value: "+ intf.method_2());
+			System.out.println("method_2 attr value: "+ intf.method_2());			
+			
+		  }catch(NoSuchMethodException e){
+			System.out.println(e.getMessage());
+		}
+	}
 	public static void main(String[] args) {
 	  this_meth();
-
+	  System.out.println();
+	  System.out.println("NB: \nthe params in this_meth2() are just used to show" +"\n"+
+			 "how annotation works with them, their are not used in method body");
+      this_meth2("theStringParam", 30); 
 	}
 
 }
